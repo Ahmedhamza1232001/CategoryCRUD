7f687614723155ff8470aa622bad2e73f26547a7
AhmedHamza <ahmedhamza7895123@gmail.com>
2022-10-03 13:38:37 +0200

    first version of project

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,94 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+    
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //what is validate AntiForgryToken
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())//add custom server side validation
+            {
+                ModelState.AddModelError("name", "the DisplayOrder Cannot match the Name ");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); //you could put a name of controller in second paramter
+            }
+           return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) 
+                return NotFound();
+            
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryFromDb = _db.Categories.SingleOrDefault(c => c.Id == id);
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //what is validate AntiForgryToken
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())//add custom server side validation
+            {
+                ModelState.AddModelError("name", "the DisplayOrder Cannot match the Name ");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); //you could put a name of controller in second paramter
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            var categoryFromDb = _db.Categories.Find(id);
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //what is validate AntiForgryToken
+        public IActionResult Delete(Category obj)
+        {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); //you could put a name of controller in second paramter
+        }
+    }
+}
---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key] //Or we couldn't use that because entity could understand that by Itself
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display order must be 1 to 100 only!!")] 
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    }
+}
